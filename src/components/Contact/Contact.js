b2d18c72239adf98f2c4727a1d8b1fdad5503da2
unknown <opokudavid446@gmail.com>
2020-08-02 10:20:13 +0100

    :wrench:

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -5,11 +5,15 @@ const Contact = () => (
   <div className="contact-container">
     <div className="contact-inner-container">
       <div>
-        <input type="text" className="textInput" />
-        <input type="text" className="textInput" />
+        <input type="text" placeholder="Email" className="textInput" />
+        <input type="text" placeholder="Subject" className="textInput" />
       </div>
-      <input type="text" className="textInput" />
-      <textarea className="textArea" />
+      <input
+        type="text"
+        placeholder="Full name"
+        className="textInput full-name"
+      />
+      <textarea placeholder="How may I help?" className="textArea" />
     </div>
   </div>
 )
